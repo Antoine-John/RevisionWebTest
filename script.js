ce6e10097f3d8d0aa89ff5be96e99d73ce670d23
Antoine-John <vagabondjunior2014@gmail.com>
2018-03-31 15:11:02 +0800

    Changed some formatting for mobile view

--- script.js
+++ script.js
@@ -11,5 +11,15 @@ $(document).ready(function(){
 		$('#dynamic2').load($(this).attr('href'));
 		$('.init').empty();
 		$('.main').css("display", "block");
+		$('#subNav').removeClass("sub_nav responsive").addClass("sub_nav");
 	});
 });
+
+function navBar() {
+    var x = document.getElementById("subNav");
+    if (x.className === "sub_nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "sub_nav";
+    }
+}
