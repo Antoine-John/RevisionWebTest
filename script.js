c4930af10c6207226d9d47cb027c832b46870ceb
Antoine-John <vagabondjunior2014@gmail.com>
2018-03-31 19:17:36 +0800

    Added inspirational quotes to main page

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ $(document).ready(function(){
 		$('.main').css("display", "block");
 		$('#subNav').removeClass("sub_nav responsive").addClass("sub_nav");
 	});
+
+	quoteArray= [
+	'Live as if you were to die tomorrow. Learn as if you were to live forever." - Gandhi', 
+	'"Education is the most powerful weapon you can use to change the world." - Nelson Mandela', 
+	'"An investment in knowledge pays the best interest." - Benjamin Franklin',
+	'"Give a man a fish and you feed him for a day; teach a man to fish and you feed him for a lifetime." - Maimonides'];
+	count = 0;
+	setInterval(function () {
+    	count++;
+    	$("#quote").fadeOut(400, function () {
+      $(this).text(quoteArray[count % quoteArray.length]).fadeIn(400);
+    });
+  }, 4000);
 });
 
 function navBar() {
